d56fe05c987ca1c9b19f5557e2904a6c93322882
Abhinash Rajput <singhabhinash4544@gmail.com>
2021-05-11 17:49:35 +0530

    https://github.com/Abhinash-Singh-4544/Intro-To-Vue.git

--- main.js
+++ main.js
@@ -2,33 +2,9 @@ const app = Vue.createApp({
     //data: fi=unction()
     data(){//here we use es6 short handling
         return{
-            cart:0,
-            product : 'Socks',
-            inStock: false,
-            selectedVariant: 0,
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_green.jpg',
-            inventory: 6,
-            details: ['50% cotton', '30% wool', '20% polyster'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ],
-            onSale: true
+            cart:0
         }
     },
-    methods: {
-        addToCart() {
-          this.cart += 1
-        },
-        removeFromCart() {
-            if (this.cart > 0) {
-                this.cart -= 1
-            }
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
+    methods: {}
 
 })
